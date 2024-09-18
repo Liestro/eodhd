7fd7880a6d02b0420a1d43737504f4628a6080c3
liestro <andrey.istomin511@gmail.com>
2024-09-18 13:48:54 +0400

    Review fixes #1

--- db_operations.py
+++ db_operations.py
@@ -16,12 +16,5 @@ def test_mongo_connection(mongo_client):
         print(e)
 
 
-def store_fundamentals_data(mongo_client, fundamental_data):
-    mongo_client.eodhd.fundamentals_data.create_index([("name", pymongo.ASCENDING)], unique=True)
-    fundamental_data["name"] = fundamental_data["General"]["Name"]
-    if not mongo_client.eodhd.fundamentals_data.count_documents({"name": fundamental_data["name"]}):
-        mongo_client.eodhd.fundamentals_data.insert_one(fundamental_data)
-
-
 def store_news_data(mongo_client, news_data):
     mongo_client.eodhd.news.insert_many(news_data)
